c319e28a1844e688080cb142d5f51f87a1171216
Don Schemmel & Annie Hoogendoorn <student@epicodus.com>
2015-09-08 16:02:26 -0700

    Add specs and function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-var triangleTracker = function()
+var triangleTracker = function(sideA, sideB, sideC) {
+  if (sideA < (sideB + sideC) && sideB < (sideA + sideC) && sideC < (sideA + sideB)) {
+    if (sideA === sideB && sideB === sideC) {
+      return "Equilateral";
+    }
+    else if (sideA === sideB || sideB === sideC || sideC === sideA){
+      return "Isosceles";
+    }
+    else if (sideA !== sideB && sideB !== sideC && sideC !== sideA) {
+      return "Scalene";
+    }
+  }
+
+  else {
+    return "These sides do not make a triangle.";
+  }
+
+};
+
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    var sideA = parseInt($("input#sideA").val());
+    var sideB = parseInt($("input#sideB").val());
+    var sideC = parseInt($("input#sideC").val());
+    var result = triangleTracker(sideA, sideB, sideC);
+
+    sideA = parseInt($("input#sideA").val(""));
+    sideB = parseInt($("input#sideB").val(""));
+    sideC = parseInt($("input#sideC").val(""));
+
+
+    $("#result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,33 @@
 describe('triangleTracker', function() {
-  it("")
+  it("if side c is greater than side A + B then it's not a triangle ", function() {
+    expect(triangleTracker(5,5,11)).to.equal("These sides do not make a triangle.");
+  });
+
+  it("if side A is greater than side B + C then it's not a triangle", function() {
+    expect(triangleTracker(11,5,5)).to.equal("These sides do not make a triangle.");
+  });
+
+  it("if side B is greater than side A + C then it's not a triangle", function() {
+    expect(triangleTracker(5,11,5)).to.equal("These sides do not make a triangle.");
+  });
+
+  it("if sides A, B and C are equal then it's an equilateral", function() {
+    expect(triangleTracker(5,5,5)).to.equal("Equilateral");
+  });
+
+  it("if sides A and B are equal then it's an isosceles", function() {
+    expect(triangleTracker(5,5,6)).to.equal("Isosceles");
+  });
+
+  It("If side B and C are equal then it's an isosceles", function() {
+    expect(triangleTracker(6,5,5)).to.equal("Isosceles");
+  });
+
+  it("if side C and A are equal then it's an isosceles", function() {
+    expect(triangleTracker(5,6,5)).to.equal("Isosceles");
+  });
+
+  it("no sides are equal then it's a scalene", function() {
+    expect(triangleTracker(4,5,6)).to.equal("Scalene");
+  });
 });
